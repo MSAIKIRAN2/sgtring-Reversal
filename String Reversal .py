8f9b092b869be14f63e9662151260ecb118df29b
MSAIKIRAN2 <143923687+MSAIKIRAN2@users.noreply.github.com>
2023-11-19 10:17:27 +0530

    Add files via upload

---
+++ String Reversal .py
@@ -0,0 +1,6 @@
+def reverse_string(input_stering):
+    reverse_string = input_string[::-1]
+    return reverse_string
+input_string = input("enter a string: ")
+reversed_result = reverse_string(input_string)
+print("Reversed string:",reversed_result)
